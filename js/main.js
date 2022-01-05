ed2fc8faabfc98fd77ae08e92515020f2946550e
Sangg <ngocsang1201@gmail.com>
2022-01-05 20:56:01 +0700

    Bind colors to li elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { getRandomColorPairs } from './colorHelper.js';
-import { GAME_STATE } from './constants.js';
+import { GAME_STATE, PAIRS_COUNT } from './constants.js';
+import { getColorItemList, getColorListElement } from './selectors.js';
 
+// Global variables
 let selections = [];
 let gameState = GAME_STATE.PLAYING;
 
@@ -11,4 +13,33 @@ let gameState = GAME_STATE.PLAYING;
 // 4. Add timer
 // 5. Handle replay click
 
-(() => {})();
+function initColorList() {
+	const colorList = getRandomColorPairs(PAIRS_COUNT);
+
+	const liList = getColorItemList();
+
+	liList.forEach((liElement, index) => {
+		const overlayElement = liElement.querySelector('.overlay');
+		if (overlayElement) overlayElement.style.backgroundColor = colorList[index];
+	});
+}
+
+function handleColorItemClick(liElement) {
+	if (!liElement) return;
+
+	liElement.classList.add('active');
+}
+
+function attachColorListClick() {
+	const colorListElement = getColorListElement();
+	if (!colorListElement) return;
+
+	colorListElement.addEventListener('click', (e) => {
+		handleColorItemClick(e.target);
+	});
+}
+
+(() => {
+	initColorList();
+	attachColorListClick();
+})();
